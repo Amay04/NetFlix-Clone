77df741c3cc03bf035a7f58f399bbc4baa006981
Amay Naik <ameyanaik2002@gmail.com>
2024-12-06 02:30:52 +0530

    initial commit

---
+++ Backend/controllers/auth.controller.js
@@ -0,0 +1,5 @@
+export const signup =  (req,res)=> res.send("Sign up route")
+
+export const login =  (req,res)=> res.send("Login route")
+
+export const logout =  (req,res)=> res.send("Log out route")
