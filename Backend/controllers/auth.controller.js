1bc8e5c77a89166c6b2f99973310e2b458c3db12
Amay Naik <ameyanaik2002@gmail.com>
2024-12-09 02:55:53 +0530

    Sign up done

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,4 +1,66 @@
-export const signup =  (req,res)=> res.send("Sign up route")
+import {User} from "../models/user.model.js";
+import bycrypt from "bcryptjs";
+import { generateTokenAndSetCookie } from "../utils/genrateToken.js";
+
+export const signup = async (req,res)=> {
+    try {
+        const {email,password,username} = req.body;
+
+        if(!email || !password || !username){
+            return res.status(400).json({success:false, message:"All fields are required"})
+        }
+        const emailRegex = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/gm;
+
+        if(!emailRegex.test(email)){
+            return res.status(400).json({success:false, message:"invalid email"})
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({success:false, message:"Password must be atleast 6 characters"})
+        }
+        
+        const existingUserByEmail = await User.findOne({email:email})
+
+        if(existingUserByEmail){
+            return res.status(400).json({success:false, message:"Email already exists"})
+        }
+        const existingUserByUsername = await User.findOne({username:username})
+
+        if(existingUserByUsername){
+            return res.status(400).json({success:false, message:"Username already exists"})
+        }
+
+        const salt = await bycrypt.genSalt(10);
+        const hashedPassword = await bycrypt.hash(password,salt);
+
+
+        const PROFILE_PIC = ["/avatar1.png","/avatar2.png","/avatar3.png"];
+
+        const image = PROFILE_PIC[Math.floor(Math.random()* PROFILE_PIC.length)];
+
+
+        const newUser = new User({
+            email,
+            password: hashedPassword,
+            username,
+            image
+        })
+
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({success:true, user:{
+                ...newUser._doc,
+                password:""
+            }})
+        
+
+      
+    } catch (error) {
+        console.log("Error in signup controller" + error.message);
+        
+        res.status(500).json({success:false, message:"Internal server error"})
+    }
+}
 
 export const login =  (req,res)=> res.send("Login route")
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,16 @@
 import express from "express";
-import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.route.js"
-
-dotenv.config();
-
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
+const PORT = ENV_VARS.PORT
 
+app.use(express.json());
 app.use("/api/v1/auth", authRoutes)
 
-app.listen(5000, ()=> console.log("Server started at http://localhost:5000"));
+app.listen(PORT, ()=> {
+    console.log("Server started at http://localhost:"+PORT)
+    connectDB();
+});
